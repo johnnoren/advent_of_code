55335d0187a82cefd9c4524738d3ec2233e488b4
John Norén <super.happy.email.fun@gmail.com>
2023-02-13 10:40:55 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("application")
+    kotlin("jvm") version "1.8.10"
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("com.example.MainApp.kt")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+include("app")
+
+rootProject.name = "Advent of code"
+
+pluginManagement {
+    repositories.gradlePluginPortal()
+}
+
+dependencyResolutionManagement {
+    repositories.mavenCentral()
+}
