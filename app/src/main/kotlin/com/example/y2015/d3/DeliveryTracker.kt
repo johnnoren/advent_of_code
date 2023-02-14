010130055cf5853c643a93ba980415f9e43f96c4
John Norén <super.happy.email.fun@gmail.com>
2023-02-14 17:31:11 +0100

    2015 day 3 refactored done

--- app/src/main/kotlin/com/example/y2015/d3/DeliveryTracker.kt
+++ app/src/main/kotlin/com/example/y2015/d3/DeliveryTracker.kt
@@ -3,52 +3,36 @@ package com.example.y2015.d3
 import java.util.InputMismatchException
 
 class DeliveryTracker {
-    fun getNumberOfHouses(directions: String, splitDirections: Boolean): Int {
-        return if (splitDirections) getNumberOfHousesFromTwoCouriers(directions) else getNumberOfHousesFromOneCourier(directions)
-    }
 
-    private fun getPositionsFromDirections(directions: String) : List<Position> {
-        var currentPosition = Position(0,0)
-        val positionList = mutableListOf(currentPosition)
-
-        directions.toList().forEach {
-            currentPosition = when(it){
-                '^' -> currentPosition.moveUp()
-                'v' -> currentPosition.moveDown()
-                '<' -> currentPosition.moveLeft()
-                '>' -> currentPosition.moveRight()
-                else -> throw InputMismatchException("Invalid character: $it")
-            }
-        positionList.add(currentPosition)
-        }
+    private fun getAllPositions(directions: String) : List<Position> {
+        val positionList = mutableListOf(Position(0,0))
+        directions.toList().forEach { positionList.add(getNextPosition(it, positionList.last())) }
         return positionList
     }
 
-    private fun getNumberOfHousesFromOneCourier(directions: String) : Int {
-        val positionList = getPositionsFromDirections(directions)
-        val uniqueHouses = positionList.toSet()
-
-        return uniqueHouses.size
+    private fun getNextPosition(character: Char, currentPosition: Position) = when (character) {
+        '^' -> currentPosition.moveUp()
+        'v' -> currentPosition.moveDown()
+        '<' -> currentPosition.moveLeft()
+        '>' -> currentPosition.moveRight()
+        else -> throw InputMismatchException("Invalid character: $character")
     }
 
-    private fun getNumberOfHousesFromTwoCouriers(directions: String) : Int {
-        val splitDirections = splitDirections(directions)
-        val courier1Positions = getPositionsFromDirections(splitDirections.first)
-        val courier2Positions = getPositionsFromDirections(splitDirections.second)
-        val mergedPositionList = courier1Positions + courier2Positions
-        val uniqueHouses = mergedPositionList.toSet()
+    fun getNumberOfHouses(directions: String, splitDirections: Boolean) : Int {
+        val directionsBatches = if (splitDirections) splitDirections(directions) else listOf(directions)
+        val positions = directionsBatches.flatMap { getAllPositions(it) }
+        val uniquePositions = positions.toSet()
 
-        return uniqueHouses.size
+        return uniquePositions.size
     }
 
-    private fun splitDirections(directions: String) : Pair<String,String> {
+    private fun splitDirections(directions: String) : List<String> {
         val even: (Int) -> Boolean = { it % 2 == 0 }
         val odd: (Int) -> Boolean = { it % 2 == 1 }
         val directionsString1 = directions.toList().filterIndexed { index, _ -> even(index) }.joinToString("")
         val directionsString2 = directions.toList().filterIndexed { index, _ -> odd(index) }.joinToString("")
-        println(directionsString1)
 
-        return Pair(directionsString1,directionsString2)
+        return listOf(directionsString1,directionsString2)
     }
 
 }
